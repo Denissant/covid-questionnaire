241bb6ca8fc286057c2507884cf4b79396214f38
Denissant <denissanturyan@gmail.com>
2022-12-21 14:58:39 +0400

    feat: add form header

--- src/components/FormWrapper.tsx
+++ src/components/FormWrapper.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import FormHeader from './FormHeader';
+
+const formRoutes = [
+  '/form/personal-details',
+  '/form/covid',
+  '/form/vaccine',
+  '/form/policy',
+];
 
 const FormWrapper = () => {
+  const { pathname } = useLocation();
+  const pageIndex = formRoutes.indexOf(pathname);
+
   return (
     <main className='relative min-h-screen py-22 px-50'>
-      <header></header>
+      <FormHeader pageNumber={pageIndex + 1} />
       <Outlet />
       <nav></nav>
     </main>
