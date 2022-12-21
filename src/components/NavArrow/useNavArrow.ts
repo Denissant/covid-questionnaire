86c3a46e05ea2384dbbefa80975f28be92cfe71c
Denissant <denissanturyan@gmail.com>
2022-12-21 17:32:43 +0400

    fix: arrow svg path

--- src/components/NavArrow/useNavArrow.ts
+++ src/components/NavArrow/useNavArrow.ts
@@ -4,7 +4,7 @@ function useNavArrow(
   isHidden: boolean
 ) {
   return (
-    'bg-[url("/assets/next_2.svg")] w-[18px] h-[23px] bg-no-repeat' +
+    'bg-[url("/assets/next.svg")] w-[18px] h-[23px] bg-no-repeat' +
     (isActive ? '' : ' opacity-50') +
     (isPrevious ? ' rotate-180' : '') +
     (isHidden ? ' invisible' : '')
