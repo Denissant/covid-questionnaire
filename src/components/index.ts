e2130bfd0ae6f1ee8a9b1b6d4f95faeda9045516
Denissant <denissanturyan@gmail.com>
2022-12-23 14:00:08 +0400

    style: radio input component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { default as Background } from './Background';
 export { default as FormHeader } from './FormHeader';
 export * from './FormWrapper';
 export { default as TextInput } from './TextInput';
+export { default as RadioInput } from './RadioInput';
+export { default as FormLabel } from './FormLabel';
