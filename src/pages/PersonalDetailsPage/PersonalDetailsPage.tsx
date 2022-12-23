655ea24af4d0971e4dcdd05f22845ad3fa100761
Denissant <denissanturyan@gmail.com>
2022-12-24 00:28:32 +0400

    feat: apply validation to personal details page

---
+++ src/pages/PersonalDetailsPage/PersonalDetailsPage.tsx
@@ -0,0 +1,70 @@
+import { TextInput } from 'components';
+
+const PersonalDetailsPage = () => {
+  return (
+    <>
+      <form className='w-min animate-fade-in'>
+        <TextInput
+          name='first_name'
+          label='სახელი*'
+          placeholder='იოსებ'
+          validationRules={{
+            required: 'სახელის ველის შევსება სავალდებულოა',
+            minLength: {
+              value: 2,
+              message: 'სახელის ველი უნდა შედგებოდეს მინიმუმ 2 სიმბოლოსგან',
+            },
+            maxLength: {
+              value: 256,
+              message: 'სახელის ველი უნდა შედგებოდეს მაქსიმუმ 256 სიმბოლოსგან',
+            },
+          }}
+        />
+        <TextInput
+          name='last_name'
+          label='გვარი*'
+          placeholder='ჯუღაშვილი'
+          validationRules={{
+            required: 'გვარის ველის შევსება სავალდებულოა',
+            minLength: {
+              value: 2,
+              message: 'გვარის ველი უნდა შედგებოდეს მინიმუმ 2 სიმბოლოსგან',
+            },
+            maxLength: {
+              value: 256,
+              message: 'გვარის ველი უნდა შედგებოდეს მაქსიმუმ 256 სიმბოლოსგან',
+            },
+          }}
+        />
+        <TextInput
+          name='email'
+          label='მეილი*'
+          placeholder='fbi@redberry.ge'
+          validationRules={{
+            required: 'მეილის ველის შევსება სავალდებულოა',
+            maxLength: {
+              value: 256,
+              message: 'მეილის ველი უნდა შედგებოდეს მაქსიმუმ 256 სიმბოლოსგან',
+            },
+            pattern: {
+              value: /^[^.][^\s@]*@redberry\.ge+$/,
+              message:
+                'შეიყვანე ვალიდური რედბერის მეილი (უნდა ბოლოვდებოდეს @redberry.ge-ით)',
+            },
+          }}
+        />
+        <hr className='bg-black w-60 mt-20 mb-5' />
+        <p className='w-72 text-brand-gray'>
+          *-ით მონიშნული ველების შევსება სავალდებულოა
+        </p>
+      </form>
+      <img
+        src='/assets/background-personal-details.png'
+        alt=''
+        className='-mt-16 -mr-8 animate-fade-in'
+      />
+    </>
+  );
+};
+
+export default PersonalDetailsPage;
