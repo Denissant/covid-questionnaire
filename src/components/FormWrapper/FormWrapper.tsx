7eceb64983519e64eb3083250a1cdcc7e511077c
Denissant <denissanturyan@gmail.com>
2022-12-24 00:24:36 +0400

    feat: setup FormProvider in form wrapper component

--- src/components/FormWrapper/FormWrapper.tsx
+++ src/components/FormWrapper/FormWrapper.tsx
@@ -1,22 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import { FormHeader, NavArrow } from 'components';
 import { useFormWrapper } from './useFormWrapper';
+import { FormProvider } from 'react-hook-form';
 
 const FormWrapper = () => {
-  const { pageIndex, formRoutes } = useFormWrapper();
+  const { pageIndex, formRoutes, form } = useFormWrapper();
 
   return (
     <main className='relative min-h-screen py-22 px-50'>
       <FormHeader pageNumber={pageIndex + 1} />
       <section className='flex items-start justify-between'>
-        <Outlet />
+        <FormProvider {...form}>
+          <Outlet />
+        </FormProvider>
       </section>
       <nav className='absolute bottom-22 left-1/2 -translate-x-1/2 w-36 flex justify-between'>
         {
           <NavArrow
             isHidden={pageIndex < 1}
             isPrevious={true}
-            isActive={true}
+            formControl={form.control}
+            triggerFormValidation={form.trigger}
             url={formRoutes[pageIndex - 1]}
           />
         }
@@ -24,7 +28,8 @@ const FormWrapper = () => {
           <NavArrow
             isHidden={pageIndex > 2}
             isPrevious={false}
-            isActive={true}
+            formControl={form.control}
+            triggerFormValidation={form.trigger}
             url={formRoutes[pageIndex + 1]}
           />
         }
