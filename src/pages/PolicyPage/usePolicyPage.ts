8cb85becd5cd48edb21fb7b8759536cc4ac2475a
Denissant <denissanturyan@gmail.com>
2022-12-25 23:52:21 +0400

    refactor: apply type to form data

--- src/pages/PolicyPage/usePolicyPage.ts
+++ src/pages/PolicyPage/usePolicyPage.ts
@@ -1,12 +1,16 @@
 import { useFormContext } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { FormFields } from 'types';
 
 export const usePolicyPage = () => {
   const { handleSubmit } = useFormContext();
   const navigate = useNavigate();
 
-  const onSubmit = async (formData: { [key: string]: string | boolean }) => {
-    for (const [key, value] of Object.entries(formData)) {
+  const onSubmit = async (formData: FormFields) => {
+    for (const [key, value] of Object.entries(formData) as [
+      keyof FormFields,
+      string
+    ][]) {
       if (key === 'had_antibody_test' || key === 'had_vaccine') {
         formData[key] = Boolean(value);
       }
