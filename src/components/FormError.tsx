1d002cdff1e335debb619492eb06a78daea41273
Denissant <denissanturyan@gmail.com>
2022-12-28 17:49:22 +0400

    fix: form errors causing layout shift

--- src/components/FormError.tsx
+++ src/components/FormError.tsx
@@ -1,16 +1,11 @@
-import { ErrorMessage } from '@hookform/error-message';
 import { useFormState } from 'react-hook-form';
 
 const FormError = (props: { name: string }) => {
   const { errors } = useFormState({ name: props.name });
   return (
-    <ErrorMessage
-      errors={errors}
-      name={props.name}
-      render={({ message }) => (
-        <p className='mt-1.5 ml-5 text-brand-orange'>{message}</p>
-      )}
-    />
+    <p className='h-6 mt-1.5 ml-5 text-brand-orange'>
+      {errors[props.name]?.message as string}
+    </p>
   );
 };
 
